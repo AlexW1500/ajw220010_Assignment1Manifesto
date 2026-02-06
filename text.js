53fb3b6c0680ecc9ca92cd007a0d76500acf2f34
AlexW1500 <alexketchumall@gmail.com>
2026-02-05 18:23:46 -0600

    Create text.js

---
+++ text.js
@@ -0,0 +1,7 @@
+console.log("                         Z          
+    N       Z
+  A     Z
+      P  Sleep...
+            S E L F C A R E B
+             I                E
+             T                D");
